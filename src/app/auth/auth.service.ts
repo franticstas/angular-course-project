fae5a554101b7f28f933073e3432d511ae4bd161
stas <franticstas@gmail.com>
2021-06-08 22:27:40 +0300

    add error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 interface AuthResponseData {
   idToken: string;
@@ -14,13 +16,28 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBs2BS4YK7pWyWJ6Ce9JlluGGitGbsbhvc',
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBs2BS4YK7pWyWJ6Ce9JlluGGitGbsbhvc',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error occured!';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email exists already';
+          }
+          return throwError(errorMessage);
+        })
+      );
   }
 }
---
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  template:
+    '<div class="loadingio-spinner-rolling-h4s4stwibk"><div class="ldio-7ggttivwgt7"><div></div></div></div>',
+  styleUrls: ['./loading-spinner.component.css'],
+})
+export class LoadingSpinnerComponent {}
